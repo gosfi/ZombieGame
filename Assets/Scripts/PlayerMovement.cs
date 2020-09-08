3dd9deee07f493c8f203d21e23bc34f1fe750ad0
RaphyPSW <49653672+RaphyPSW@users.noreply.github.com>
2020-09-08 16:12:40 -0400

    problem fix, rotate and move

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public CharacterController controller;
+
+    void Move()
+    {
+        float posX = Input.GetAxis("Horizontal");
+        float posZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * posX + transform.forward * posZ;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
