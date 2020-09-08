3f5e12386ce32beceee81b741ebfe3cd3d04733c
gosfi <samLef12@outlook.com>
2020-09-08 10:47:17 -0400

    created class gun

---
+++ Assets/Scripts/Guns/Gun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun
+{
+    int nbOfBullets;
+    float range, dmg;
+
+    public virtual void Shoot()
+    {
+
+    }
+
+    public virtual void Reload()
+    {
+
+    }
+
+    public virtual void Aim()
+    {
+
+    }
+}
