3780e4001d310fffef6541da9c4da86653d90867
RaphyPSW <49653672+RaphyPSW@users.noreply.github.com>
2020-09-08 10:53:51 -0400

    player move ready

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int speed = 10;
+
+
+
+    Vector3 move()
+    {
+        Vector3 direction = new Vector3();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction.x += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction.z += speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction.z -= speed;
+
+        }
+        return direction;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += move() * Time.deltaTime;
+    }
+}
