f902a6d9660c9bb67634ae2272b77d680a003cc5
gosfi <samLef12@outlook.com>
2020-09-10 12:26:34 -0400

    reseted gun branch

---
+++ Assets/Scripts/Guns/MuzzleFlash.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleFlash : MonoBehaviour
+{
+    public GameObject flashHolder;
+    public Sprite[] flashSprites;
+    public SpriteRenderer[] spritesRenderers;
+
+    public float flashTime;
+
+    private void Start()
+    {
+        Deactivate();
+    }
+
+    public void Activate()
+    {
+        flashHolder.SetActive(true);
+
+        int flashSpriteIndex = Random.Range(0, flashSprites.Length);
+        for (int i = 0; i < spritesRenderers.Length; ++i)
+        {
+            spritesRenderers[i].sprite = flashSprites[flashSpriteIndex];
+        }
+
+        Invoke("Deactivate", flashTime);
+    }
+
+    private void Deactivate()
+    {
+        flashHolder.SetActive(false);
+    }
+}
