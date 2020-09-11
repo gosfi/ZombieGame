b031f0819c8a5800ee846a13e30300b015027dc4
RaphyPSW <49653672+RaphyPSW@users.noreply.github.com>
2020-09-11 13:04:07 -0400

    enemy pathfinding

---
+++ Assets/Scripts/Zombie/EnemyMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField]
+    Transform destination;
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(navMeshAgent == null)
+        {
+            Debug.LogError("Le nav mesh est pas attacher a " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+        
+    }
+
+    private void SetDestination()
+    {
+        if(destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+   
+}
