590794629f74bb97a1668b7c43ec6278fa7acef4
RaphyPSW <49653672+RaphyPSW@users.noreply.github.com>
2020-09-13 10:32:53 -0400

    enemyisHit

---
+++ Assets/Scripts/Zombie/EnemySettings.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySettings : MonoBehaviour
+{
+    public bool isHit = false;
+
+    float hp = 100f;
+    float receiveDamage = 10f;
+    
+    bool isDead = false;
+
+    public GameObject zombie;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Zombie HP : " + hp);
+        Hit();
+        Dead();
+    }
+
+    void Hit()
+    {
+        if (isHit)
+        {
+            hp -= receiveDamage;
+            isHit = false;
+        }
+        if(hp <= 0)
+        {
+            isDead = true;
+        }
+    }
+
+    void Dead()
+    {
+
+        if (isDead)
+        {
+            Destroy(zombie);  
+        }
+
+    }
+}
