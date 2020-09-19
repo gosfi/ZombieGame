9799c9e9495697a77762c2104cc2dba968dfc366
RaphyPSW <49653672+RaphyPSW@users.noreply.github.com>
2020-09-19 10:34:31 -0400

    shop script test

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -6,12 +6,24 @@ using UnityEngine.UI;
 public class Shop : MonoBehaviour
 {
     bool active = false;
+    bool shotgunActive = false;
+    bool ak47Active = false;
 
-    public GameObject panel; 
+    public int playerMoney;
+    public int buyMoney;
+    public Text playerMoneyText;
+    public Text buyText;
+    public GameObject panel;
+    public GameObject line1;
+    public GameObject line2;
+    public PlayerInventory player;
+    public GameObject shotgun;
+    public GameObject ak47;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerMoney = 0;
     }
 
     // Update is called once per frame
@@ -30,5 +42,41 @@ public class Shop : MonoBehaviour
 
             panel.SetActive(active);
         }
+
+        playerMoneyText.text = playerMoney.ToString();
+    }
+
+    public void SelectShotgun()
+    {
+        shotgunActive = true;
+        ak47Active = false;
+
+        line1.SetActive(shotgunActive);
+        line2.SetActive(ak47Active);
+        buyText.text = "500";
+    }
+    public void SelectAk47()
+    {
+        shotgunActive = false;
+        ak47Active = true;
+
+        line1.SetActive(shotgunActive);
+        line2.SetActive(ak47Active);
+        buyText.text = "1 000";
+
+    }
+
+    public void Buy() { 
+        
+        if(shotgunActive && playerMoney >= 500)
+        {
+            player.guns[1] = shotgun;
+        }
+
+        if(ak47Active && playerMoney >= 1000)
+        {
+            player.guns[1] = ak47;
+        }
+
     }
 }
