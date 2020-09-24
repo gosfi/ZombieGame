ac79a3983fb73fd9cbae4cbdb90db8bcdbbd1036
RaphyPSW <49653672+RaphyPSW@users.noreply.github.com>
2020-09-24 15:03:54 -0400

    testing online

---
+++ Assets/Scripts/OnlineRaph/TestPlayer.cs
@@ -0,0 +1,25 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : NetworkBehaviour
+{
+    [SerializeField] private Vector3 movement = new Vector3();
+
+    [Client]
+    private void Update()
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+
+        if (!Input.GetKeyDown(KeyCode.Space))
+        {
+            return;
+        }
+
+        transform.Translate(movement);
+    }
+}
