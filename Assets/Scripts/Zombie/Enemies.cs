a13b56dd48db0b1fc2822bde98a7ca19270c7512
gosfi <samLef12@outlook.com>
2020-09-29 15:56:23 -0400

    merge demo

---
+++ Assets/Scripts/Zombie/Enemies.cs
@@ -0,0 +1,144 @@
+﻿using Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemies : MonoBehaviour
+{
+
+    float hp = 100f;
+    // float receiveDamage = 10f;
+    bool isInDistance = true;
+    float distance;
+    float hitTimer;
+    float attackRange = 0.5f;
+    float distanceRange = 0.8f;
+
+    protected float timer = 2f;
+    protected float damage = 2f;
+    protected float speed = 3.5f;
+
+    public NavMeshAgent agent;
+    public GameObject zombie;
+    public Animator animator;
+    public Transform attackPoint;
+    public Transform distancePoint;
+    public LayerMask playerLayers;
+    private Transform player;
+
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("criss").transform;
+    }
+
+    private void Start()
+    {
+        hitTimer = timer;
+        agent.speed = speed;
+
+
+
+        Debug.Log($"is the player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance();
+    }
+
+    public void Hit(float dmg)
+    {
+        hp -= dmg;
+
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetBool("isAttacking", true);
+        animator.SetTrigger("Attack");
+        Debug.Log("start Attack method");
+        Collider[] hitPlayer = Physics.OverlapSphere(attackPoint.position, attackRange, playerLayers);
+
+        foreach (Collider players in hitPlayer)
+        {
+
+            PlayerMovement player = players.GetComponent<PlayerMovement>();
+
+            player.Hit(damage);
+            Debug.Log(players.name + "s'est fait hit");
+        }
+    }
+
+    public virtual void Distance()
+    {
+        Debug.Log("start distance method");
+        distance = Vector3.Distance(distancePoint.position, player.position);
+
+        Collider[] distancePlayer = Physics.OverlapSphere(distancePoint.position, distanceRange, playerLayers);
+
+        foreach (Collider players in distancePlayer)
+        {
+            if (distance > 2)
+            {
+
+                break;
+
+            }
+            agent.speed = 0;
+            hitTimer -= Time.deltaTime;
+            if (isInDistance)
+            {
+                Debug.Log("deuxieme if calisse");
+                Attack();
+                isInDistance = false;
+            }
+            if (hitTimer <= 0)
+            {
+                Debug.Log("troisieme if esti");
+
+                Attack();
+                hitTimer = timer;
+
+            }
+
+        }
+        if (distance > 2)
+        {
+            animator.SetBool("isAttacking", false);
+            animator.SetTrigger("Walk");
+            hitTimer = timer;
+            agent.speed = speed;
+            isInDistance = true;
+
+        }
+    }
+
+
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        if (distancePoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        Gizmos.DrawWireSphere(distancePoint.position, distanceRange);
+    }
+
+    void Dead()
+    {
+        Destroy(zombie);
+    }
+}
