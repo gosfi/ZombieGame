89240838b36aac0128e313393ddc81b7bd7beaec
Samuel Lefrancois <slefranc@isimtl.local>
2020-10-14 14:10:56 -0400

    okÈ

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -59,40 +59,12 @@ namespace Player
 
         void Update()
         {
-            //  if (isLocalPlayer)
-            // {
-            // Move();
-            // RotatePlayer();
+           
             Gravity();
             CheckGround();
             Regen();
             Down();
             Revive();
-            //  }
-        }
-
-        void Move()
-        {
-            float posX = Input.GetAxis("Horizontal");
-            float posZ = Input.GetAxis("Vertical");
-
-            Vector3 move = transform.right * posX + transform.forward * posZ;
-
-            controller.Move(move * speed * Time.deltaTime);
-
-            if (Input.GetKey(KeyCode.LeftShift) && !isDown)
-            {
-                speed = RUN_SPEED;
-            }
-            else
-            {
-                speed = WALK_SPEED;
-            }
-
-            if (isDown)
-            {
-                speed /= WALK_SPEED;
-            }
         }
 
         void Gravity()
@@ -111,17 +83,6 @@ namespace Player
             }
         }
 
-        void RotatePlayer()
-        {
-            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-            xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-            camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-            player.Rotate(Vector3.up * mouseX);
-        }
 
         public void Hit(float dmgReceived)
         {
